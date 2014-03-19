acf3dfb1efb1703b90066232dd02d90ca1ce5747
clayschemer <martin.rydin@gmail.com>
2014-03-19 15:30:59 +0100

    changed return types in database_interface

--- in_memory.h
+++ in_memory.h
@@ -1,16 +1,20 @@
 #ifndef IN_MEMORY_H
 #define IN_MEMORY_H
 
+#include <string>
+#include <vector>
 #include "database_interface.h"
 
 class InMemory {
 public:
 	bool createNG(const NewsGroup&);
 	bool removeNG(const NewsGroup&);
-	NewsGroup[] listNGs() const;
-	Article[] listArticles(const NewsGroup&) const;
+	std::vector<NewsGroup> listNGs() const;
+	std::vector<Article> listArticles(const NewsGroup&) const;
 	bool addArticle(const NewsGroup&, const Article&);
-	bool removeArticle(const NewsGroup&, const Article&);	
+	bool removeArticle(const NewsGroup&, const Article&);
+private:
+	std::vector<std::vector<Article>> news_groups;
 };
 
 #endif
