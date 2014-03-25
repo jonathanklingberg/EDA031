4cb1ad301d8641ade0e59b3ade3611f2c2e70e6e
Martin Rydin <martin.rydin@gmail.com>
2014-03-25 16:23:50 +0100

    Added stubs in in_memory.cc

---
+++ in_memory.cc
@@ -0,0 +1,39 @@
+#include "in_memory.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+InMemory::InMemory() {}
+
+bool InMemory::createNG(const NewsGroup&) {
+	
+}
+	
+bool InMemory::removeNG(const NewsGroup&) {
+	
+}
+	
+vector<NewsGroup> InMemory::listNGs() const {
+	
+}
+	
+vector<Article> InMemory::listArticles(const NewsGroup&) const {
+	
+}
+	
+bool InMemory::addArticle(const NewsGroup&, const Article&) {
+	
+}
+	
+bool InMemory::removeArticle(const NewsGroup&, const Article&) {
+	
+}
+	
+	
+	
+	
+	
+	
+	
