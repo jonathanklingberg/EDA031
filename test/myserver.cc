c1d522a5ff2b033d574db5000a09540a04dfcc9e
FredrikHelander <gda10fhe@student.lu.se>
2014-03-28 11:13:49 +0100

    progress on myserver

--- test/myserver.cc
+++ test/myserver.cc
@@ -4,6 +4,9 @@
 #include "connectionclosedexception.h"
 #include "servercommandhandler.h"
 #include "protocol.h"
+#include "in_memory.h"
+#include "newsgroup.h"
+#include "database_interface.h"
 
 #include <memory>
 #include <iostream>
@@ -11,6 +14,7 @@
 #include <stdexcept>
 #include <sstream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
@@ -38,36 +42,40 @@ int main(int argc, char* argv[]){
 		auto conn = server.waitForActivity();
         MessageHandler smh(*conn.get());
         ServerCommandHandler sch(smh);
-        // CREATE A DB HERE : Database db();
+        DatabaseInterface db();  // CREATE A DB HERE : Database db();
 		if (conn != nullptr) {
 			try {
                 unsigned char command = sch.readCommand();
                 unsigned char end_command;
-                map<int, string> groups;
+                vector<NewsGroup> newsgoups = db.listNGs();
+                map<size_t, string> groups;
                 switch (command) {
                     case Protocol::COM_LIST_NG: // list newsgroups
                         end_command = sch.readCommand();
                         // check endcommand...
-                        //                      map<int, string> groups = db.listNgs();
-                        groups.insert(make_pair(0,"C++ gruppen"));
-                        groups.insert(make_pair(1,"Java gruppen"));
+                        //sch.writeAnswer(Protocol::PAR_NUM);
+                        //sch.writeNumber(newsgroups.size());    // Antal newsgroups
+                        for(size_t i; i < newsgroups.size(); ++i) {                        	
+                        	groups.insert(make_pair(newsgroups[i].getId(), newsgroups[i].getTitle()));
+                        }
                         sch.writeAnswer(Protocol::ANS_LIST_NG);
                         sch.writeMap(groups);
                         break;
                     case Protocol::COM_CREATE_NG: // create newsgroup
-                    	unsigned char param = sch.readCommand();
+                    	sch.readCommand(); //PAR_STRING
                     	int namesize = sch.readNumber();
                     	string groupName = smh.readString(namesize);
                     	//int res = db.addGroup(groupId);
-                    	end_command = sch.readCommand();
-                    	map<int, string> db;                      // fake db
-                    	db.insert(make_pair(0, groupName));
+                    	end_command = sch.readCommand();                  
+                    	db.createNG(groupName);
                         //                    	cout << "db atm: " << endl;
                         //    			for(auto it = db.cbegin(); it != db.cend(); ++it){
                         //        		std::cout << it->first << " " << it->second << "\n";
                         //    			}
                         sch.writeAnswer(Protocol::ANS_CREATE_NG);
-                        if (true) {
+                        if (find_if(newsgroups.begin(), newsgroups.end(), [string groupName](NewsGroup ng){ return ng.getTitle() != groupName}) {  //lambda
+                            Newsgroup ng(groupName);
+                            db.createNG(ng);
                             sch.writeAnswer(Protocol::ANS_ACK);
                         }else{
                         sch.writeAnswer(Protocol::ANS_NAK);
@@ -78,11 +86,18 @@ int main(int argc, char* argv[]){
                     case Protocol::COM_DELETE_NG : // deletes a specified newsgroup
                         sch.readCommand();
                         int groupId = sch.readNumber();
-                        //int res = db.getRem(groupId);
-                        //sch.writeNumber(res);
                         sch.readCommand();
+                        
                         sch.writeAnswer(Protocol::ANS_DELETE_NG);
-                        if(true){
+                        //NewsGroup romove_group;
+                        /*
+                     	for(size_t i = 0; i < newsgroups.size(); ++i) {
+                     		if(newsgroups[i].getId() == groupId) {
+                     			remove_group = newsgroups[i];
+                     		}
+                        }
+               		*/
+                        if(db.removeNG(groupId)){               // Anropa databas metod
                             sch.writeAnswer(Protocol::ANS_ACK);
                         }else{
                             sch.writeAnswer(Protocol::ANS_NAK);
@@ -90,11 +105,35 @@ int main(int argc, char* argv[]){
                         }
                         break;
                     case Protocol::COM_LIST_ART: // list articles
+                        sch.readCommand(); //PAR_NUM
+                        int groupId = sch.readNumber();   //dvs newsgroup ID
+                        sch.readCommand(); //COM_END
                         
+                        sch.writeAnswer(Protocol::ANS_LIST_ART);
+                         if(find_if(newsgroups.begin(), newsgroups.end(), [string groupName](NewsGroup ng){ return ng.getId() == groupId}) {
+                            sch.writeAnswer(Protocol::ANS_ACK);	
+                            vector<Article> articles = db.listArticles(groupId);
+                            sch.writeAnswer(Protocol::PAR_NUM);
+                            sch.writeNumber(articles.size());                 //Antal artiklar
+                            for(size_t i = 0; i < articles.size(); ++i) {
+                            	sch.writeAnswer(Protocol::PAR_NUM);
+                            	sch.writeNumber(i);
+                            	sch.writeAnswer(Protocol::PAR_STRING);
+                            	sch.writeNumber(articles[i].getTitle().size());
+                            	sch.writeString(articles[i].getTitle());
+                            }         
+                        }else{
+                            sch.writeAnswer(Protocol::ANS_NAK);
+                            sch.writeAnswer(Protocol::ERR_NG_DOES_NOT_EXIST);
+                        }
                         break;
                         
                     case Protocol::COM_CREATE_ART: // create article
-                        
+                        	sch.readCommand();   // PAR_NUM
+                        	int groupId = sch.readNumber();   // N 
+                        	sch.readCommand(); // PAR_STRING
+                        	
+                        	
                         break;
                         
                     case Protocol::COM_DELETE_ART: // delete article
--- test/myserver.d
+++ test/myserver.d
@@ -1,3 +1,4 @@
-myserver.o myserver.d : myserver.cc ../server.h ../connection.h \
-  ../connectionclosedexception.h ../servercommandhandler.h \
-  ../messagehandler.h ../protocol.h
+myserver.o myserver.d : myserver.cc ../server.h ../connection.h ../connection.h \
+ ../connectionclosedexception.h ../servercommandhandler.h \
+ ../messagehandler.h ../protocol.h ../in_memory.h ../database_interface.h \
+ ../newsgroup.h ../article.h ../database_interface.h
