ff671478d1c13baaea7291016713ce26233dc041
Martin Rydin <martin.rydin@gmail.com>
2014-04-02 12:48:59 +0200

    added cases in db_test

--- clientcommandhandler.d
+++ clientcommandhandler.d
@@ -1,2 +1,2 @@
 clientcommandhandler.o clientcommandhandler.d : clientcommandhandler.cc clientcommandhandler.h \
- messagehandler.h connection.h protocol.h
+  messagehandler.h connection.h protocol.h
