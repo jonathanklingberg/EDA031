dde5228eaee68df7cffbb59ea41b894b2a533f97
David Lundberg <davidlundberg91@gmail.com>
2014-04-10 14:12:07 +0200

    all unnecessary classes removed

--- test/myclient.h
+++ test/myclient.h
@@ -21,17 +21,26 @@ public:
     
     vector<string> validate_input(string& param);
     
-    const string instructions = "please see the commands below: write the command followed by any paramaters, end with carriage return";
     const string list = "list newsgroups";
-    const string create_group = "create newsgroup [newsgroup title]";
-    const string delete_group = "delete newsgroup [newsgroup id]";
-    const string list_articles = "list articles [newsgroup id]";
-    const string create_article = "create article [newsgroup id] [title] [author] [text]";
-    const string delete_article = "delete article [newsgroup id] [article id]";
-    const string get_article = "read article [newsgroup id] [article id]";
+    const string create_group = "create newsgroup";
+    const string delete_group = "delete newsgroup";
+    const string list_articles = "list articles";
+    const string create_article = "create article";
+    const string delete_article = "delete article";
+    const string get_article = "read article";
     const string help = "-h";
     
-    vector<string> commandlist = {instructions,list,create_group,delete_group,list_articles,create_article,delete_article,get_article};
+    const string instr = "please see the commands below: write the command followed by any paramaters, end with carriage return";
+    const string ls_grps = "list newsgroups";
+    const string new_grp = "create newsgroup [newsgroup title]";
+    const string del_grp = "delete newsgroup [newsgroup id]";
+    const string ls_arts = "list articles [newsgroup id]";
+    const string new_art = "create article [newsgroup id] [title] [author] [text]";
+    const string del_art = "delete article [newsgroup id] [article id]";
+    const string get_art = "read article [newsgroup id] [article id]";
+
+    
+    vector<string> commandlist = {instr,ls_grps,new_grp,del_grp,ls_arts,new_art,del_art,get_art};
     
 };
 
